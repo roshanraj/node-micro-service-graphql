60df006ea7b985ed7c6faeebc59bfbcd7edf0663
roshanraj <roshanraj@live.com>
2019-07-12 08:56:28 +0530

    simple course service

--- app.js
+++ app.js
@@ -28,17 +28,78 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', index); // this to be replaced with spa 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+        coursebyauthor(author: String): [Course]
+    },
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    }
 `);
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
+var coursesData = [
+  {
+      id: 1,
+      title: 'The Complete Node.js Developer Course',
+      author: 'Andrew Mead, Rob Percival',
+      description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+      topic: 'Node.js',
+      url: 'https://codingthesmartway.com/courses/nodejs/'
   },
+  {
+      id: 2,
+      title: 'Node.js, Express & MongoDB Dev to Deployment',
+      author: 'Brad Traversy',
+      description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+      topic: 'Node.js',
+      url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+  },
+  {
+      id: 3,
+      title: 'JavaScript: Understanding The Weird Parts',
+      author: 'Anthony Alicea',
+      description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+      topic: 'JavaScript',
+      url: 'https://codingthesmartway.com/courses/understand-javascript/'
+  }
+]
+
+var getCourse = function(args) { 
+  var id = args.id;
+  return coursesData.filter(course => {
+      return course.id == id;
+  })[0];
+}
+var getCourses = function(args) {
+  if (args.topic) {
+      var topic = args.topic;
+      return coursesData.filter(course => course.topic === topic);
+  } else {
+      return coursesData;
+  }
+}
+
+
+var getAuthor = function(args) {
+  if (args.author) {
+      var author = args.author;
+      return coursesData.filter(course => course.author === author);
+  } else {
+      return coursesData;
+  }
+}
+var root = {
+  course: getCourse,
+  courses: getCourses,
+  coursebyauthor: getAuthor,
 };
+
 // graphql api
 app.use('/graphql/v1/hacker', graphqlHTTP({
   schema: schema,
